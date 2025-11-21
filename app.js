7ac5dbbb47df7074edf17257a1311c8ece8f8067
DharmaDuraiV <dharmadurai280@gmail.com>
2025-11-21 12:48:14 +0530

    commit

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import fs from "node:fs";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -8,4 +9,15 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+app.get("/home", (req, res) => {
+  res.send("I am from Home Page");
+});
+
+app.get("/api/v1/products", (req, res) => {
+  fs.readFile("./data.json", "utf-8", (err, data) => {
+    const parseData = JSON.parse(data);
+    if (err) throw err;
+    res.status(200).json({ status: "success", products: { parseData } });
+  });
+});
 export default app;
